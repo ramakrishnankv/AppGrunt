ff027e0d39394781a47021c1ea6268c4cd80c31f
Ramakrishnan K V <ramakrishnankv@yahoo.com>
2015-12-11 21:42:52 +0530

    Change model to use js instead of JSON

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,5 +1,5 @@
 module.exports.controller = function(app) {
-    var data = require(app.get('models') + 'home.json');
+    var data = require(app.get('models') + 'home.js');
     app.get("/", function(req, res, next) {
         // doo all the logic with data
         res.render('home', {data: data, title: 'Test'});
--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,5 +1,5 @@
 module.exports.controller = function(app) {
-    var data = require(app.get('models') + 'login.json');
+    var data = require(app.get('models') + 'login.js');
     app.get("/login", function(req, res, next) {
         // doo all the logic with data
         res.render('login', {data: data, title: 'Test'});
