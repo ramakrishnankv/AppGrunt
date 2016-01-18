da22666667bcfbb62d654aeb5bdfbd183fda6e5a
Ramakrishnan KV <Ramakrishnan.kv@orbitz.com>
2016-01-18 12:20:26 -0600

    Login Data - Dynamic JSON creation. Stringify and Parse

--- src/models/login.js
+++ src/models/login.js
@@ -1,6 +1,10 @@
-module.exports = {
-    "page": {
-        "title": "Please Login"
-    },
-    "name": "Login Page"
+module.exports = function(app) {
+	var loginModel = {};
+	var names = ['Login'];
+	var page = {};
+	page.title = 'Please Login';
+	loginModel.page = page;
+	loginModel.name = names[0];
+
+	return JSON.stringify(loginModel);
 };
